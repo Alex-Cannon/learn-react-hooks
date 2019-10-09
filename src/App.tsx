cb5101a264ee732c7503c37543399f444e262e6d
alex-cannon <alexcannon2018@gmail.com>
2019-10-09 11:44:46 -0600

    feat: learned useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import { UseStateExample } from './hooks/useState/useState';
+
+const HOOKS = [{
+  title: "I hereby declare useState!",
+  component: UseStateExample
+}];
+
 const App: React.FC = () => {
+  let [index, setIndex] = useState(0);
+
+  const HookExample = HOOKS[index].component;
+
   return (
     <div className="App">
-      <h1>Learn React Hooks!</h1>
+      <h1>Get Hooked on React Hooks!</h1>
+      <h4>Lesson #{index + 1}: {HOOKS[index].title}</h4>
+      <HookExample/>
     </div>
   );
 }
---
+++ src/hooks/useState/useState.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+// Function Syntax / using Hooks
+export default () => {
+  let [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Add 1</button>
+    </>
+  );
+}
+
+// Class Syntax
+// Note: We're using TypeScript, so it's a little different than usual
+type Props = {};
+
+type State = {
+  count: number
+};
+
+export class UseStateExample extends React.Component<Props, State> {
+  public readonly state: State = {
+    count: 0
+  }
+  
+  setCount(count: number) {
+    this.setState({ count });
+  }
+
+  public render () {
+    let { count } = this.state;
+
+    return (
+      <>
+      <p>{count}</p>
+        <button onClick={() => this.setCount(count + 1)}>Add 1</button>
+      </>
+    );
+  }
+}
